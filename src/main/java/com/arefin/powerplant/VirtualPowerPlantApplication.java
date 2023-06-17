a75a7143eca9ae4db8f7dd89b9b55dd3717a26a7
arefin <arefin.sust@gmail.com>
2023-06-17 14:53:38 +0600

    Refactor - remove unused code.

--- src/main/java/com/arefin/powerplant/VirtualPowerPlantApplication.java
+++ src/main/java/com/arefin/powerplant/VirtualPowerPlantApplication.java
@@ -8,7 +8,6 @@ public class VirtualPowerPlantApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VirtualPowerPlantApplication.class, args);
-        System.out.println("I am working!!!");
     }
 
 }
